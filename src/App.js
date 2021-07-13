21836f2dd82e342b8fa7d10b89e3bfafe6c5243a
br00klu <brooklu9@gmail.com>
2021-07-13 10:34:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import imageInSrc from './Assets/imageInSrc.png'
+import myVideo from './Assets/myVideo.mp4'
+import '../src/styles.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{border:'solid black 1px',maxWidth:'100vw'}}>
+
+        <h1 className="title red">brokko</h1>
+
+        <br/>
+        <img src={imageInSrc} alt="sad brokko" width="320px" height="240px"/>
+
+        <br/>
+
+        <img src="/imageInPublic.png" alt="happy brokko" />
+
+      </div>
+
+      <video style={{width:"320px",height:"240px"}}>
+
+        <source src={myVideo} type="video/mp4" />
+
+      </video>
+
     </div>
   );
 }
